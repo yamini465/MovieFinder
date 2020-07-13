5880db49c55d3ba89c1ab54f08d32156da673b59
yamini465 <40388091+yamini465@users.noreply.github.com>
2020-07-13 15:20:48 +0530

    Updates

---
+++ precache-manifest.8c7ebe82b5683433e30836efff0fcbee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2867b084413c804f11c9328e3a7c407b",
+    "url": "/MovieFinder/index.html"
+  },
+  {
+    "revision": "6a53d4bbed8e6efd54d9",
+    "url": "/MovieFinder/static/css/main.d498bfb3.chunk.css"
+  },
+  {
+    "revision": "0b7c8f0849eb370c1cdf",
+    "url": "/MovieFinder/static/js/2.1e5218ed.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/MovieFinder/static/js/2.1e5218ed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6a53d4bbed8e6efd54d9",
+    "url": "/MovieFinder/static/js/main.2800b9aa.chunk.js"
+  },
+  {
+    "revision": "25bbe12c42773d66fbf2",
+    "url": "/MovieFinder/static/js/runtime-main.77b6194c.js"
+  }
+]);
